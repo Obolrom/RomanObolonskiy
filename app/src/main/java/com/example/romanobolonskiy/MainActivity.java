8ddfc993b9f9c4c64a1e366ef7e05fced7cb8721
Obolrom <obolrom@gmail.com>
2020-10-27 13:52:28 +0200

    second activity
    second activity was designed, and one button is working

--- app/src/main/java/com/example/romanobolonskiy/MainActivity.java
+++ app/src/main/java/com/example/romanobolonskiy/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,14 +14,24 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button taskBtn = findViewById(R.id.add_task_button);
 
+        taskBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent("create.new.task"));
+            }
+        });
     }
 
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuha, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
--- app/src/main/java/com/example/romanobolonskiy/SecondActivity.java
+++ app/src/main/java/com/example/romanobolonskiy/SecondActivity.java
@@ -10,23 +10,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
-    private TextView textView;
-    private String   gotMessage;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Intent intent = getIntent();
-        gotMessage = intent.getStringExtra("justString");
-
-        textView = findViewById(R.id.activity_second_text_view1);
-        if (gotMessage.isEmpty()) {
-            textView.setText("THE STRING WAS EMPTY");
-        } else {
-            textView.setText(gotMessage);
-        }
 
     }
 
